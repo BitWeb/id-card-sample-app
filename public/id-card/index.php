2530c8250b2c7cd246ad978214fc31eb612afd10
Rain Ramm <rammrain@gmail.com>
2014-04-10 22:14:11 +0300

    Remove personal inforamtion.

--- public/id-card/index.php
+++ public/id-card/index.php
@@ -10,8 +10,9 @@ Zend\Mvc\Application::init(require 'config/application.config.php');
 
 $redirectUrl = urldecode($_GET["redirectUrl"]);
 
-$_SERVER[IdCardAuthentication::SSL_CLIENT_VERIFY] = IdCardAuthentication::SSL_CLIENT_VERIFY_SUCCESSFUL;
-$_SERVER['SSL_CLIENT_S_DN'] = 'GN=Rain/SN=Ramm/serialNumber=39211192796/C=EST';
+// for testing use the following to mock auth result
+//$_SERVER[IdCardAuthentication::SSL_CLIENT_VERIFY] = IdCardAuthentication::SSL_CLIENT_VERIFY_SUCCESSFUL;
+//$_SERVER['SSL_CLIENT_S_DN'] = 'GN=Mari-Liis/SN=Männik/serialNumber=47101010033/C=EST';
 
 if (!IdCardAuthentication::isSuccessful()) {
     $redirectUrl = '/id-card/no-card-found';
